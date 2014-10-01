dd3c0632067d9417d892d5d176f870a72e2b5e96
Dan Gebhardt <dan@cerebris.com>
2014-10-01 14:32:34 -0400

    Clear out redundant commented-out tests

--- tests/test-module-for-model-test.js
+++ tests/test-module-for-model-test.js
@@ -95,38 +95,38 @@ test('model is using the WhazzitAdapter', function() {
   ok(store.adapterFor(model.constructor) instanceof WhazzitAdapter);
 });
 
-////TODO - model.save() promise is never fulfilled
-////       (broken on this branch as well as on master)
-////
-//// if (DS._setupContainer) {
-////   test('creates the custom adapter', function() {
-////     expect(2);
-////     ok(!whazzitCreateRecordCalled, 'precond - custom adapter is not yet instantiated');
-////
-////     var model = this.subject();
-////
-////     return Ember.run(function(){
-////       model.set('gear', '42');
-////       return model.save().then(function(){
-////         ok(whazzitCreateRecordCalled, 'uses the custom adapter');
-////       });
-////     });
-////   });
-//// } else {
-////   test('without DS._setupContainer fails to create the custom adapter', function() {
-////     var thrown = false;
-////     try {
-////       var model = this.subject();
-////       Ember.run(function(){
-////         model.set('gear', '42');
-////         return model.save();
-////       });
-////     } catch(e) {
-////       thrown = true;
-////     }
-////     ok(thrown, 'error is thrown without DS._setupContainer');
-////   });
-//// }
+//TODO - model.save() promise is never fulfilled
+//       (broken on this branch as well as on master)
+//
+// if (DS._setupContainer) {
+//   test('creates the custom adapter', function() {
+//     expect(2);
+//     ok(!whazzitCreateRecordCalled, 'precond - custom adapter is not yet instantiated');
+//
+//     var model = this.subject();
+//
+//     return Ember.run(function(){
+//       model.set('gear', '42');
+//       return model.save().then(function(){
+//         ok(whazzitCreateRecordCalled, 'uses the custom adapter');
+//       });
+//     });
+//   });
+// } else {
+//   test('without DS._setupContainer fails to create the custom adapter', function() {
+//     var thrown = false;
+//     try {
+//       var model = this.subject();
+//       Ember.run(function(){
+//         model.set('gear', '42');
+//         return model.save();
+//       });
+//     } catch(e) {
+//       thrown = true;
+//     }
+//     ok(thrown, 'error is thrown without DS._setupContainer');
+//   });
+// }
 
 moduleForModel('whazzit', 'moduleForModel whazzit with application adapter', {
   preSetup: function() {
@@ -142,75 +142,3 @@ test('model is using the ApplicationAdapter', function() {
   ok(store.adapterFor(model.constructor) instanceof ApplicationAdapter);
   ok(!(store.adapterFor(model.constructor) instanceof WhazzitAdapter));
 });
-
-////moduleForComponent('x-foo', 'moduleForComponent with x-foo');
-////
-////test('renders', function() {
-////  expect(2);
-////  var component = this.subject();
-////  equal(component._state, 'preRender');
-////  this.render();
-////  equal(component._state, 'inDOM');
-////});
-////
-////test('append', function() {
-////  expect(3);
-////  var component = this.subject();
-////  equal(component._state, 'preRender');
-////  this.append();
-////  equal(component._state, 'inDOM');
-////  ok(Ember.A(Ember.deprecationWarnings).contains('this.append() is deprecated. Please use this.render() instead.'));
-////});
-////
-////test('yields', function() {
-////  expect(2);
-////  var component = this.subject({
-////    layout: "yield me".compile()
-////  });
-////  equal(component._state, 'preRender');
-////  this.render();
-////  equal(component._state, 'inDOM');
-////});
-////
-////test('can lookup components in its layout', function() {
-////  expect(1);
-////  var component = this.subject({
-////    layout: "{{x-foo id='yodawg-i-heard-you-liked-x-foo-in-ur-x-foo'}}".compile()
-////  });
-////  this.render();
-////  equal(component._state, 'inDOM');
-////});
-////
-////test('clears out views from test to test', function() {
-////  expect(1);
-////  var component = this.subject({
-////    layout: "{{x-foo id='yodawg-i-heard-you-liked-x-foo-in-ur-x-foo'}}".compile()
-////  });
-////  this.render();
-////  ok(true, 'rendered without id already being used from another test');
-////});
-////
-////moduleForComponent('pretty-color', 'moduleForComponent with pretty-color');
-////
-////test("className", function(){
-////  // first call to this.$() renders the component.
-////  ok(this.$().is('.pretty-color'));
-////});
-////
-////test("template", function(){
-////  var component = this.subject();
-////
-////  equal($.trim(this.$().text()), 'Pretty Color:');
-////
-////  Ember.run(function(){
-////    component.set('name', 'green');
-////  });
-////
-////  equal($.trim(this.$().text()), 'Pretty Color: green');
-////});
-////
-////test("$", function(){
-////  var component = this.subject({name: 'green'});
-////  equal($.trim(this.$('.color-name').text()), 'green');
-////  equal($.trim(this.$().text()), 'Pretty Color: green');
-////});
