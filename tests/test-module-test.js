8092ce00bfd6b68ce3d1b2ce77562d1fc546dd1c
Dan Gebhardt <dan@cerebris.com>
2014-10-01 15:29:31 -0400

    Test preSetup and preTeardown callbacks

--- tests/test-module-test.js
+++ tests/test-module-test.js
@@ -13,19 +13,35 @@ var registry = {
 };
 
 var a = 0;
+var b = 0;
+var preSetupOk = false;
+var preTeardownOk = false;
 
 moduleFor('component:x-foo', 'TestModule callbacks', {
   preSetup: function() {
     setResolverRegistry(registry);
+
+    preSetupOk = (a === 0);
+    b += 1;
   },
 
   setup: function() {
     a += 1;
   },
 
+  preTeardown: function() {
+    preTeardownOk = (a === 1);
+    b -= 1;
+  },
+
   teardown: function() {
     a -= 1;
   }
+});
+
+test("preSetup callback is called prior to any test setup", function() {
+  ok(preSetupOk);
+  equal(b, 1);
 });
 
 test("setup callback is called prior to test", function() {
@@ -34,4 +50,9 @@ test("setup callback is called prior to test", function() {
 
 test("teardown callback is called after test", function() {
   equal(a, 1);
+});
+
+test("preTeardown callback is called prior to any test teardown", function() {
+  ok(preTeardownOk);
+  equal(b, 1);
 });
