b8a96f2ad622573003a4993c8d30d750714a8ee4
Dan Gebhardt <dan@cerebris.com>
2014-10-01 13:38:42 -0400

    Organize Brocfile and asset output

--- Brocfile.js
+++ Brocfile.js
@@ -2,15 +2,11 @@ var compileES6 = require('broccoli-es6-concatenator');
 var pickFiles  = require('broccoli-static-compiler');
 var mergeTrees = require('broccoli-merge-trees');
 
+// --- Main lib and tests ---
+
 var loader = pickFiles('bower_components', {
   srcDir: 'loader',
   files: ['loader.js'],
-  destDir: '/assets'
-});
-
-var testShims = pickFiles('bower_components', {
-  srcDir: 'ember-cli-shims',
-  files: ['app-shims.js'],
   destDir: '/assets'
 });
 
@@ -28,14 +24,10 @@ var main = compileES6(libAndTests, {
   loaderFile: 'assets/loader.js',
   inputFiles: ['**/*.js'],
   ignoredModules: ['ember'],
-  outputFile: '/js/ember-test-helpers.amd.js'
+  outputFile: '/assets/ember-test-helpers.amd.js'
 });
 
-var qunit = pickFiles('bower_components', {
-  srcDir: '/qunit/qunit',
-  files: ['qunit.js', 'qunit.css'],
-  destDir: '/assets'
-});
+// --- Vendor ---
 
 var jQuery = pickFiles('bower_components', {
   srcDir: 'jquery/dist',
@@ -61,9 +53,25 @@ var emberData = pickFiles('bower_components', {
   destDir: '/assets'
 });
 
+var vendor = mergeTrees([jQuery, handlebars, ember, emberData]);
+
+// --- Test support ---
+
+var qunit = pickFiles('bower_components', {
+  srcDir: '/qunit/qunit',
+  files: ['qunit.js', 'qunit.css'],
+  destDir: '/assets'
+});
+
 var testLoader = pickFiles('bower_components', {
   srcDir: 'ember-cli-test-loader',
   files: ['test-loader.js'],
+  destDir: '/assets'
+});
+
+var testShims = pickFiles('bower_components', {
+  srcDir: 'ember-cli-shims',
+  files: ['app-shims.js'],
   destDir: '/assets'
 });
 
@@ -73,7 +81,7 @@ var testIndex = pickFiles('tests', {
   destDir: '/tests'
 });
 
-var assets = mergeTrees([testLoader, testIndex, qunit, jQuery, handlebars, loader, ember, emberData, testShims]);
+var testSupport = mergeTrees([qunit, testLoader, testShims, testIndex]);
 
-module.exports = mergeTrees([main, assets]);
+module.exports = mergeTrees([testSupport, vendor, main]);
 
