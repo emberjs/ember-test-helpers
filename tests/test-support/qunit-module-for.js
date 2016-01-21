e60117dec9516d02ddcc5f86d345eae6f1a2c27a
Matt Eddy <matteddy1@gmail.com>
2016-01-20 20:12:53 -0800

    Add moduleForAcceptance

--- tests/test-support/qunit-module-for.js
+++ tests/test-support/qunit-module-for.js
@@ -2,11 +2,11 @@ export default function qunitModuleFor(module) {
   QUnit.module(module.name, {
     setup: function(assert) {
       var done = assert.async();
-      module.setup()['finally'](done);
+      module.setup(assert)['finally'](done);
     },
     teardown: function(assert) {
       var done = assert.async();
-      module.teardown()['finally'](done);
+      module.teardown(assert)['finally'](done);
     }
   });
 }
