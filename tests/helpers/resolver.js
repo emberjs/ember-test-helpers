4ba79eb28a6959af97014b64d5f41cc6438dff93
Robert Jackson <rjackson@linkedin.com>
2017-10-09 09:01:55 -0400

    Ensure that users of resolver have proper event dispatcher.

--- tests/helpers/resolver.js
+++ tests/helpers/resolver.js
@@ -4,6 +4,7 @@ import { dasherize } from '@ember/string';
 import AppResolver from '../../resolver';
 import config from '../../config/environment';
 import { setResolver } from 'ember-test-helpers';
+import require from 'require';
 
 const Resolver = AppResolver.extend({
   registry: {},
@@ -33,6 +34,17 @@ export default {
 };
 
 export function createCustomResolver(registry) {
+  if (require.has('ember-native-dom-event-dispatcher')) {
+    // the raw value looked up by ember and these test helpers
+    registry[
+      'event_dispatcher:main'
+    ] = require('ember-native-dom-event-dispatcher').default;
+    // the normalized value looked up
+    registry[
+      'event-dispatcher:main'
+    ] = require('ember-native-dom-event-dispatcher').default;
+  }
+
   var Resolver = Ember.DefaultResolver.extend({
     registry: null,
 
