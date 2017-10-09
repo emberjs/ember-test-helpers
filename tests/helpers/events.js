97997f1d0886b4e091173fc051ce8c9c5585a7f8
Robert Jackson <rjackson@linkedin.com>
2017-10-09 08:33:46 -0400

    Add native event helpers.
    Mostly copied/stolen from emberjs's packages/ember-testing/lib/events.js
    but with some tweaks to make it work without jQuery (I think those
    changes were made upstream in Ember as well).
    
    The long term goal here is to remove this file and have
    ember-test-helpers _include_ the helpers from ember-native-dom-helpers,
    but that will require an RFC once the changes proposed in
    emberjs/rfcs#230 are implemented.

---
+++ tests/helpers/events.js
@@ -0,0 +1,134 @@
+import { run } from '@ember/runloop';
+import { merge } from '@ember/polyfills';
+
+const DEFAULT_EVENT_OPTIONS = { canBubble: true, cancelable: true };
+const KEYBOARD_EVENT_TYPES = ['keydown', 'keypress', 'keyup'];
+const MOUSE_EVENT_TYPES = [
+  'click',
+  'mousedown',
+  'mouseup',
+  'dblclick',
+  'mouseenter',
+  'mouseleave',
+  'mousemove',
+  'mouseout',
+  'mouseover',
+];
+
+export const elMatches =
+  typeof Element !== 'undefined' &&
+  (Element.prototype.matches ||
+    Element.prototype.matchesSelector ||
+    Element.prototype.mozMatchesSelector ||
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.oMatchesSelector ||
+    Element.prototype.webkitMatchesSelector);
+
+export function matches(el, selector) {
+  return elMatches.call(el, selector);
+}
+
+export function focus(el) {
+  if (!el) {
+    return;
+  }
+  if (matches(el, 'input, textarea, select, button, [contenteditable=true]')) {
+    let type = el.type;
+    if (type !== 'checkbox' && type !== 'radio' && type !== 'hidden') {
+      run(function() {
+        // Firefox does not trigger the `focusin` event if the window
+        // does not have focus. If the document doesn't have focus just
+        // use trigger('focusin') instead.
+
+        if (!document.hasFocus || document.hasFocus()) {
+          el.focus();
+        }
+      });
+    }
+  }
+}
+
+export function fireEvent(element, type, options = {}) {
+  if (!element) {
+    return;
+  }
+  let event;
+  if (KEYBOARD_EVENT_TYPES.indexOf(type) > -1) {
+    event = buildKeyboardEvent(type, options);
+  } else if (MOUSE_EVENT_TYPES.indexOf(type) > -1) {
+    let rect = element.getBoundingClientRect();
+    let x = rect.left + 1;
+    let y = rect.top + 1;
+    let simulatedCoordinates = {
+      screenX: x + 5,
+      screenY: y + 95,
+      clientX: x,
+      clientY: y,
+    };
+    event = buildMouseEvent(type, merge(simulatedCoordinates, options));
+  } else {
+    event = buildBasicEvent(type, options);
+  }
+  element.dispatchEvent(event);
+}
+
+function buildBasicEvent(type, options = {}) {
+  let event = document.createEvent('Events');
+  event.initEvent(type, true, true);
+  merge(event, options);
+  return event;
+}
+
+function buildMouseEvent(type, options = {}) {
+  let event;
+  try {
+    event = document.createEvent('MouseEvents');
+    let eventOpts = merge({}, DEFAULT_EVENT_OPTIONS);
+    merge(eventOpts, options);
+
+    event.initMouseEvent(
+      type,
+      eventOpts.canBubble,
+      eventOpts.cancelable,
+      window,
+      eventOpts.detail,
+      eventOpts.screenX,
+      eventOpts.screenY,
+      eventOpts.clientX,
+      eventOpts.clientY,
+      eventOpts.ctrlKey,
+      eventOpts.altKey,
+      eventOpts.shiftKey,
+      eventOpts.metaKey,
+      eventOpts.button,
+      eventOpts.relatedTarget
+    );
+  } catch (e) {
+    event = buildBasicEvent(type, options);
+  }
+  return event;
+}
+
+function buildKeyboardEvent(type, options = {}) {
+  let event;
+  try {
+    event = document.createEvent('KeyEvents');
+    let eventOpts = merge({}, DEFAULT_EVENT_OPTIONS);
+    merge(eventOpts, options);
+    event.initKeyEvent(
+      type,
+      eventOpts.canBubble,
+      eventOpts.cancelable,
+      window,
+      eventOpts.ctrlKey,
+      eventOpts.altKey,
+      eventOpts.shiftKey,
+      eventOpts.metaKey,
+      eventOpts.keyCode,
+      eventOpts.charCode
+    );
+  } catch (e) {
+    event = buildBasicEvent(type, options);
+  }
+  return event;
+}
