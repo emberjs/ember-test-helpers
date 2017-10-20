emberjs/ember-test-helpers

commit 7ad93d2c5331279b798055b46034c92da031f1e7

Ensure testing elements are properly reset/cleared.
Prior to this change, the `innerHTML` of the `#ember-testing` element was reset
after each test (much in the same way as QUnit itself resets the `qunit-fixture`
between tests). There were two specific issues with the approach:

1. The `teardownRenderingContext` was **required** to run after the
  `teardownContext`. If the order was set correctly (`teardownRenderContext(this)`
  _then_ `teardownContext(this)`) errors would be thrown because Ember is
  attempting to clean up the rendering engine and its expected DOM elements to
  remove are no longer present. This meant that we could not use the normal rule
  of thumb for these things: that teardown is done in the reverse order of setup.
2. Any attributes or properties added to the `#ember-testing` element were
  **not** being cleaned up, and caused a large number of cascading test
  failures. Ember's acceptance tests set an `ember-application` class on what it
  thinks is the `rootElement`, if it sees that class is already present it
  throws an error. Due to the way tests were being improperly cleaned up, one
  failed acceptance test (which failed in such a way as proper cleanup could not
  happen) would essentially cause **all** of the remaining acceptance tests to
  fail.

The fix here was to swap from capturing and resetting the `#ember-testing`
elements `innerHTML` to capturing and resetting the `#ember-testing-container`'s `innerHTML`.
This has the effect of ensuring that the `#ember-testing` element (which is
contained _within_ the `#ember-testing-container` element) is reset completely,
and allows Ember's rendering system to continue to do its normal cleanup without
error (because the elements it expected to exist are still present).

addon-test-support/setup-rendering-context.js
@@ -39,13 +39,12 @@ export function clearRender() {
 export default function(context) {
   let guid = guidFor(context);
 
-  let rootTestElement = document.getElementById('ember-testing');
-  let fixtureResetValue = rootTestElement.innerHTML;
+  let testElementContainer = document.getElementById('ember-testing-container');
+  let fixtureResetValue = testElementContainer.outerHTML;
 
   RENDERING_CLEANUP[guid] = [
     () => {
-      rootTestElement.innerHTML = fixtureResetValue;
-      element = undefined;
+      testElementContainer.innerHTML = fixtureResetValue;
     },
   ];
 

tests/unit/settled-test.js
@@ -185,8 +185,8 @@ module('settle', function(hooks) {
     this.server.shutdown();
     await settled();
 
-    teardownContext(this);
     teardownRenderingContext(this);
+    teardownContext(this);
   });
 
   test('it works when async exists in `init`', async function(assert) {

tests/unit/setup-rendering-context-test.js
@@ -39,8 +39,8 @@ module('setupRenderingContext', function(hooks) {
   });
 
   hooks.afterEach(function() {
-    teardownContext(this);
     teardownRenderingContext(this);
+    teardownContext(this);
   });
 
   test('render exposes an `.element` property', async function(assert) {

tests/unit/teardown-rendering-context-test.js
@@ -1,0 +1,53 @@
+import { module, test } from 'qunit';
+import {
+  setupContext,
+  setupRenderingContext,
+  teardownContext,
+  teardownRenderingContext,
+} from 'ember-test-helpers';
+import hasEmberVersion from 'ember-test-helpers/has-ember-version';
+
+module('setupRenderingContext', function(hooks) {
+  if (!hasEmberVersion(2, 4)) {
+    return;
+  }
+
+  hooks.beforeEach(function() {
+    setupContext(this);
+    setupRenderingContext(this);
+  });
+
+  hooks.afterEach(function() {
+    teardownContext(this);
+  });
+
+  test('clears any attributes added to the ember-testing div', function(assert) {
+    let beforeTeardownEl = document.getElementById('ember-testing');
+    beforeTeardownEl.setAttribute('data-was-set', '');
+
+    assert.ok(
+      beforeTeardownEl.hasAttribute('data-was-set'),
+      'precond - attribute is present before teardown'
+    );
+    assert.ok(document.contains(beforeTeardownEl), 'precond - ember-testing element is in DOM');
+
+    teardownRenderingContext(this);
+
+    let afterTeardownEl = document.getElementById('ember-testing');
+
+    assert.notOk(
+      afterTeardownEl.hasAttribute('data-was-set'),
+      'attribute is not present on ember-testing that is in DOM'
+    );
+    assert.ok(document.contains(afterTeardownEl), 'ember-testing element is still in DOM');
+
+    assert.ok(
+      beforeTeardownEl.hasAttribute('data-was-set'),
+      'attribute is still present on prior ember-testing element after teardown'
+    );
+    assert.notOk(
+      document.contains(beforeTeardownEl),
+      'previous ember-testing element is no longer in DOM'
+    );
+  });
+});

commit 20c2cc221db70dc9e4ed139ad916ec87f8fb4b98

Ensure ember-testing-container is reset properly.
The changes made in 7ad93d2c introduced a bug in the way that the
testing DOM is cleaned up between tests. The error was quite simple:
we were storing the `storedValue = container.outerHTML` before
starting the test then resetting `container.innerHTML = storedValue`.
This created a DOM node leak for each test processed.

This fixes the issue for both the new testing API's in 0.7 and the
older pre-existing system. It also adds some post-test checks to
ensure that the DOM is indeed what we expect after each test is
completed.

addon-test-support/legacy-0-6-x/abstract-test-module.js
@@ -104,15 +104,22 @@ export default class {
   }
 
   setupTestElements() {
+    let testElementContainer = document.querySelector('#ember-testing-container');
+    if (!testElementContainer) {
+      testElementContainer = document.createElement('div');
+      testElementContainer.setAttribute('id', 'ember-testing-container');
+      document.body.appendChild(testElementContainer);
+    }
+
     let testEl = document.querySelector('#ember-testing');
     if (!testEl) {
       let element = document.createElement('div');
       element.setAttribute('id', 'ember-testing');
 
-      document.body.appendChild(element);
+      testElementContainer.appendChild(element);
       this.fixtureResetValue = '';
     } else {
-      this.fixtureResetValue = testEl.innerHTML;
+      this.fixtureResetValue = testElementContainer.innerHTML;
     }
   }
 
@@ -171,7 +178,7 @@ export default class {
   }
 
   teardownTestElements() {
-    document.getElementById('ember-testing').innerHTML = this.fixtureResetValue;
+    document.getElementById('ember-testing-container').innerHTML = this.fixtureResetValue;
 
     // Ember 2.0.0 removed Ember.View as public API, so only do this when
     // Ember.View is present

addon-test-support/setup-rendering-context.js
@@ -40,7 +40,7 @@ export default function(context) {
   let guid = guidFor(context);
 
   let testElementContainer = document.getElementById('ember-testing-container');
-  let fixtureResetValue = testElementContainer.outerHTML;
+  let fixtureResetValue = testElementContainer.innerHTML;
 
   RENDERING_CLEANUP[guid] = [
     () => {

tests/index.html
@@ -24,9 +24,7 @@
     <div id="qunit"></div>
     <div id="qunit-fixture"></div>
 
-    <div id="ember-testing-container">
-      <div id="ember-testing"></div>
-    </div>
+    <div id="ember-testing-container"><div id="ember-testing"></div></div>
 
     <script src="/testem.js" integrity=""></script>
     <script src="{{rootURL}}assets/vendor.js"></script>

tests/test-helper.js
@@ -46,6 +46,16 @@ QUnit.testDone(function({ module, name }) {
       `Ember.testing should be reset after test has completed. ${module}: ${name} did not reset Ember.testing`
     );
   }
+
+  // this is used to ensure that the testing container is always reset properly
+  let testElementContainer = document.getElementById('ember-testing-container');
+  let actual = testElementContainer.innerHTML;
+  let expected = `<div id="ember-testing"></div>`;
+  if (actual !== expected) {
+    throw new Error(
+      `Expected #ember-testing-container to be reset after ${module}: ${name}, but was \`${actual}\``
+    );
+  }
 });
 
 QUnit.assert.noDeprecations = function(callback) {

tests/unit/legacy-0-6-x/test-module-test.js
@@ -398,24 +398,20 @@ test('ember-testing content should be reset to ""', function(assert) {
 QUnit.module('ember-testing resets to non-empty value');
 
 test('sets ember-testing content to "<div>foobar</div>"', function(assert) {
-  assert.expect(0);
+  assert.expect(1);
   document.getElementById('ember-testing').innerHTML = '<div>foobar</div>';
-});
-
-test('sets ember-testing content to ""', function(assert) {
-  assert.expect(0);
 
   testModule = new TestModule('component:x-foo', 'Foo');
   testModule.setContext(this);
-  return testModule.setup(...arguments).then(() => {
-    document.getElementById('ember-testing').innerHTML = '';
+  return testModule
+    .setup(...arguments)
+    .then(() => {
+      document.getElementById('ember-testing').innerHTML = '';
 
-    return testModule.teardown(...arguments);
-  });
-});
-
-test('ember-testing content should be reset to "<div>foobar</div>"', function(assert) {
-  assert.expect(1);
-  assert.equal(document.getElementById('ember-testing').innerHTML, '<div>foobar</div>');
-  document.getElementById('ember-testing').innerHTML = '';
+      return testModule.teardown(...arguments);
+    })
+    .then(() => {
+      assert.equal(document.getElementById('ember-testing').innerHTML, '<div>foobar</div>');
+      document.getElementById('ember-testing').innerHTML = '';
+    });
 });