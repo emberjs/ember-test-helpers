1e505a9de65b6a6352abc16698df7e3eb816c151
Robert Jackson <rjackson@linkedin.com>
2017-11-11 13:46:49 -0500

    Automatically set resolver when `setApplication` is called.
    This allows an application to call:
    
    ```js
    setApplication(App.create({ autoboot: false }));
    ```
    
    And still support legacy style `moduleFor*` tests.
    
    Without this change the following both of the following invocations are
    required:
    
    ```js
    setApplication(App.create({ autoboot: false }));
    setResolver(resolver);
    ```

--- addon-test-support/@ember/test-helpers/application.js
+++ addon-test-support/@ember/test-helpers/application.js
@@ -1,7 +1,16 @@
+import { getResolver, setResolver } from './resolver';
+
 var __application__;
 
 export function setApplication(application) {
   __application__ = application;
+
+  if (!getResolver()) {
+    let Resolver = application.Resolver;
+    let resolver = Resolver.create({ namespace: application });
+
+    setResolver(resolver);
+  }
 }
 
 export function getApplication() {
---
+++ tests/unit/application-test.js
@@ -0,0 +1,43 @@
+import { module, test } from 'qunit';
+import { application, resolver } from '../helpers/resolver';
+import { setApplication, setResolver, getResolver } from '@ember/test-helpers';
+
+module('application', function(hooks) {
+  hooks.beforeEach(function() {
+    setApplication(null);
+    setResolver(null);
+  });
+
+  hooks.afterEach(function() {
+    setApplication(application);
+    setResolver(resolver);
+  });
+
+  test('calling setApplication sets resolver when resolver is unset', function(assert) {
+    setApplication(application);
+
+    let actualResolver = getResolver();
+    assert.ok(
+      actualResolver instanceof application.Resolver,
+      'a resolver instance was created from the provided application'
+    );
+    assert.equal(actualResolver.namespace, application, 'resolver has proper namespace specified');
+    assert.notOk(
+      actualResolver.isResolverFromTestHelpers,
+      'should not have used resolver created in tests/helpers/resolver.js'
+    );
+  });
+
+  test('calling setApplication when a resolver is set does not clobber existing resolver', function(
+    assert
+  ) {
+    setResolver(resolver);
+    setApplication(application);
+
+    let actualResolver = getResolver();
+    assert.ok(
+      actualResolver.isResolverFromTestHelpers,
+      'should have used resolver created in tests/helpers/resolver.js'
+    );
+  });
+});
