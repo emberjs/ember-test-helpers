5672fc13d27f5826e7e661b3ce351b45178d5911
Robert Jackson <rjackson@linkedin.com>
2017-12-05 09:43:16 -0500

    Ensure `$.ajax` is wrapped in an RSVP.Promise.
    Some `settled` unit tests fail on Ember 2.4 without this (because the
    promises are resolved and trigger an auto-run causing the post
    condition assertions to fail).

--- tests/helpers/ajax.js
+++ tests/helpers/ajax.js
@@ -1,10 +1,17 @@
+import { Promise } from 'rsvp';
 import hasjQuery from '../helpers/has-jquery';
 import $ from 'jquery'; // FYI - not present in all scenarios
 import require from 'require';
 
 export default function ajax(url) {
   if (hasjQuery()) {
-    return $.ajax(url, { cache: false });
+    return new Promise((resolve, reject) => {
+      $.ajax(url, {
+        success: resolve,
+        error: reject,
+        cache: false,
+      });
+    });
   } else {
     let fetch = require('fetch').default;
     return fetch(url).then(response => response.text());
