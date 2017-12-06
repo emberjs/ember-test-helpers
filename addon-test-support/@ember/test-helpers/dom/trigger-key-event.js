457333e5e3a84905a22e4a79ea5866003e664e0d
Robert Jackson <rjackson@linkedin.com>
2017-12-06 14:58:03 -0500

    Refactor triggerKeyEvent to be more readable.

--- addon-test-support/@ember/test-helpers/dom/trigger-key-event.js
+++ addon-test-support/@ember/test-helpers/dom/trigger-key-event.js
@@ -1,23 +1,27 @@
 import { merge } from '@ember/polyfills';
 import triggerEvent from './trigger-event';
 
+const DEFAULT_MODIFIERS = Object.freeze({
+  ctrlKey: false,
+  altKey: false,
+  shiftKey: false,
+  metaKey: false,
+});
+
 /**
   @public
-  @param selector
-  @param type
-  @param keyCode
-  @param modifiers
-  @return {*}
+  @param {String|HTMLElement} selector
+  @param {'keydown' | 'keyup' | 'keypress'} type
+  @param {String} keyCode
+  @param {Object} modifiers
+  @param {Boolean} modifiers.ctrlKey
+  @param {Boolean} modifiers.altKey
+  @param {Boolean} modifiers.shiftKey
+  @param {Boolean} modifiers.metaKey
+  @return {Promise<void>}
 */
-export default function keyEvent(
-  selectorOrElement,
-  type,
-  keyCode,
-  modifiers = { ctrlKey: false, altKey: false, shiftKey: false, metaKey: false }
-) {
-  return triggerEvent(
-    selectorOrElement,
-    type,
-    merge({ keyCode, which: keyCode, key: keyCode }, modifiers)
-  );
+export default function keyEvent(selectorOrElement, type, keyCode, modifiers = DEFAULT_MODIFIERS) {
+  let options = merge({ keyCode, which: keyCode, key: keyCode }, modifiers);
+
+  return triggerEvent(selectorOrElement, type, options);
 }
