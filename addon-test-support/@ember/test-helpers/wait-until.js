043c4bc9fc6fa76983bd74a760047171933d10f7
Robert Jackson <rjackson@linkedin.com>
2017-12-06 18:23:55 -0500

    Use `nextTick` in `waitUntil`.

--- addon-test-support/@ember/test-helpers/wait-until.js
+++ addon-test-support/@ember/test-helpers/wait-until.js
@@ -1,22 +1,29 @@
 import { Promise } from 'rsvp';
 
+import { nextTick } from './-utils';
+
 export default function(callback, options = {}) {
   let timeout = 'timeout' in options ? options.timeout : 1000;
   let waitUntilTimedOut = new Error('waitUntil timed out');
 
   return new Promise(function(resolve, reject) {
-    let time = 0;
+    // starting at -10 because the first invocation happens on 0
+    // but still increments the time...
+    let time = -10;
     function tick() {
       time += 10;
       let value = callback();
       if (value) {
         resolve(value);
       } else if (time < timeout) {
+        // using `setTimeout` directly to allow fake timers
+        // to intercept
         setTimeout(tick, 10);
       } else {
         reject(waitUntilTimedOut);
       }
     }
-    setTimeout(tick, 10);
+
+    nextTick(tick);
   });
 }
