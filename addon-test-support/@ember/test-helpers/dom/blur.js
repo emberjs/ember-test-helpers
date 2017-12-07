58441b06a0218d49e29ddb716c90d8154fe440ac
Robert Jackson <rjackson@linkedin.com>
2017-12-06 23:04:38 -0500

    Refactor `blur`
    * ensure always async
    * fire correct events (in order)
    
    To ensure correct event order, used the following snippet:
    
    ```js
    let element = document.createElement('input');
    
    ['blur', 'focusout'].forEach(type => {
      element.addEventListener(type, () => {
        console.log('event:', type);
      });
    });
    
    document.body.appendChild(element);
    ```

--- addon-test-support/@ember/test-helpers/dom/blur.js
+++ addon-test-support/@ember/test-helpers/dom/blur.js
@@ -2,6 +2,23 @@ import getElement from './-get-element';
 import fireEvent from './fire-event';
 import settled from '../settled';
 import isFocusable from './-is-focusable';
+import { nextTick } from '../-utils';
+
+export function _blur(element) {
+  let browserIsNotFocused = document.hasFocus && !document.hasFocus();
+
+  // makes `document.activeElement` be `body`.
+  // If the browser is focused, it also fires a blur event
+  element.blur();
+
+  // Chrome/Firefox does not trigger the `blur` event if the window
+  // does not have focus. If the document does not have focus then
+  // fire `blur` event via native event.
+  if (browserIsNotFocused) {
+    fireEvent(element, 'blur', { bubbles: false });
+    fireEvent(element, 'focusout');
+  }
+}
 
 /**
   @method blur
@@ -11,23 +28,15 @@ import isFocusable from './-is-focusable';
 */
 export default function blur(selectorOrElement = document.activeElement) {
   let element = getElement(selectorOrElement);
-
-  if (isFocusable(element)) {
-    let browserIsNotFocused = document.hasFocus && !document.hasFocus();
-
-    fireEvent(element, 'focusout');
-
-    // makes `document.activeElement` be `body`.
-    // If the browser is focused, it also fires a blur event
-    element.blur();
-
-    // Chrome/Firefox does not trigger the `blur` event if the window
-    // does not have focus. If the document does not have focus then
-    // fire `blur` event via native event.
-    if (browserIsNotFocused) {
-      fireEvent(element, 'blur', { bubbles: false });
-    }
+  if (!element) {
+    throw new Error(`Element not found when calling \`blur('${selectorOrElement}')\`.`);
   }
+
+  if (!isFocusable(element)) {
+    throw new Error(`${selectorOrElement} is not focusable`);
+  }
+
+  nextTick(() => _blur(element));
 
   return settled();
 }
