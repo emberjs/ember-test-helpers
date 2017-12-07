fbd035380c882acb0f855e1f9c609c141a40b12a
Robert Jackson <rjackson@linkedin.com>
2017-12-07 10:27:26 -0500

    Flesh out `triggerEvent` input validation.

--- addon-test-support/@ember/test-helpers/dom/trigger-event.js
+++ addon-test-support/@ember/test-helpers/dom/trigger-event.js
@@ -6,13 +6,26 @@ import { nextTickPromise } from '../-utils';
 /**
   @method triggerEvent
   @param {String|HTMLElement} selector
-  @param {String} type
+  @param {String} eventType
   @param {Object} options
   @return {Promise<void>}
   @public
 */
 export default function triggerEvent(selectorOrElement, type, options) {
+  if (!selectorOrElement) {
+    throw new Error('Must pass an element or selector to `triggerEvent`.');
+  }
+
   let element = getElement(selectorOrElement);
+  if (!element) {
+    throw new Error(
+      `Element not found when calling \`triggerEvent('${selectorOrElement}', ...)\`.`
+    );
+  }
+
+  if (!type) {
+    throw new Error(`Must provide an \`eventType\` to \`triggerEvent\``);
+  }
 
   return nextTickPromise().then(() => {
     fireEvent(element, type, options);
