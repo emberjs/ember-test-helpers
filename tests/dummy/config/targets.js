7a658bac26c5bd0596b9a375a7bec0e54b8c8c52
Robert Jackson <rjackson@linkedin.com>
2017-12-15 15:50:31 -0500

    Minor cleanup to address PR feedback.
    Fixes linting (to pass CI).

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,5 +1,5 @@
 /* eslint-env node */
-const browsers = ['ie 11', 'last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
+const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
 
 const isCI = !!process.env.CI;
 const isProduction = process.env.EMBER_ENV === 'production';
