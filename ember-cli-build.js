868997bfa2ddb938715fb7db6584b2ac1ec1c549
cibernox <miguel.camba@gmail.com>
2017-12-15 16:13:18 +0100

    Make tests at least boot in IE and sort package.json alphabetically

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -32,6 +32,7 @@ module.exports = function(defaults) {
   });
 
   app.import('vendor/shims/qunit.js', { type: 'test' });
+  app.import('vendor/promise-polyfill.js', { type: 'test' });
 
   return app.toTree();
 };
--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -1,4 +1,13 @@
 /* eslint-env node */
+const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
+
+const isCI = !!process.env.CI;
+const isProduction = process.env.EMBER_ENV === 'production';
+
+if (isCI || isProduction) {
+  browsers.push('ie 9');
+}
+
 module.exports = {
-  browsers: ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'],
+  browsers,
 };
