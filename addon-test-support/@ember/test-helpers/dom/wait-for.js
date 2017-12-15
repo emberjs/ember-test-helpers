1848f83f748c1503332e4047f7a372a998de8d8a
Robert Jackson <rjackson@linkedin.com>
2017-12-14 19:26:45 -0500

    Validate `target` in `waitFor`.

--- addon-test-support/@ember/test-helpers/dom/wait-for.js
+++ addon-test-support/@ember/test-helpers/dom/wait-for.js
@@ -1,6 +1,7 @@
 import waitUntil from '../wait-until';
 import { getContext } from '../setup-context';
 import getElement from './-get-element';
+import { nextTickPromise } from '../-utils';
 
 function toArray(nodelist) {
   let array = new Array(nodelist.length);
@@ -19,19 +20,25 @@ function toArray(nodelist) {
   @param {number} [options.count=1]
   @returns {Element|Array<Element>}
 */
-export default function waitFor(selector, { timeout = 1000, count = null } = {}) {
-  let callback;
-  if (count !== null) {
-    callback = () => {
-      let context = getContext();
-      let rootElement = context && context.element;
-      let elements = rootElement.querySelectorAll(selector);
-      if (elements.length === count) {
-        return toArray(elements);
-      }
-    };
-  } else {
-    callback = () => getElement(selector);
-  }
-  return waitUntil(callback, { timeout });
+export default function waitFor(target, { timeout = 1000, count = null } = {}) {
+  return nextTickPromise().then(() => {
+    if (!target) {
+      throw new Error('Must pass an element or selector to `waitFor`.');
+    }
+
+    let callback;
+    if (count !== null) {
+      callback = () => {
+        let context = getContext();
+        let rootElement = context && context.element;
+        let elements = rootElement.querySelectorAll(target);
+        if (elements.length === count) {
+          return toArray(elements);
+        }
+      };
+    } else {
+      callback = () => getElement(target);
+    }
+    return waitUntil(callback, { timeout });
+  });
 }
