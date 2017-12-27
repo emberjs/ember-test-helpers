90ba39cdd08dec287c31c306496379928839626d
Robert Jackson <rjackson@linkedin.com>
2017-12-27 16:16:47 -0500

    Docs: flesh out tap documentation

--- addon-test-support/@ember/test-helpers/dom/tap.js
+++ addon-test-support/@ember/test-helpers/dom/tap.js
@@ -4,12 +4,14 @@ import { __click__ } from './click';
 import settled from '../settled';
 import { nextTickPromise } from '../-utils';
 
-/*
-  @method tap
-  @param {String|Element} target
-  @param {Object} options
-  @return {Promise}
+/**
+  Taps on the specified selector.
+
   @public
+  @method tap
+  @param {String|Element} target the element or selector to tap on
+  @param {Object} options the options to be provided to the touch events
+  @return {Promise<void>} resolves when the application is settled
 */
 export default function tap(target, options = {}) {
   return nextTickPromise().then(() => {
