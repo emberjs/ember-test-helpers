c1c1e46508de75997f3833d05f0b6a81f4d6ff48
Robert Jackson <rjackson@linkedin.com>
2017-12-29 10:34:50 -0500

    Docs: document application utility functions

--- addon-test-support/@ember/test-helpers/application.js
+++ addon-test-support/@ember/test-helpers/application.js
@@ -2,6 +2,16 @@ import { getResolver, setResolver } from './resolver';
 
 var __application__;
 
+/**
+  Stores the provided application instance so that tests being ran will be aware of the application under test.
+
+  - Required by `setupApplicationContext` method.
+  - Used by `setupContext` and `setupRenderingContext` when present.
+
+  @public
+  @method setApplication
+  @param {Ember.Application} application the application that will be tested
+*/
 export function setApplication(application) {
   __application__ = application;
 
@@ -13,6 +23,13 @@ export function setApplication(application) {
   }
 }
 
+/**
+  Retrieve the application instance stored by `setApplication`.
+
+  @public
+  @method getApplication
+  @returns {Ember.Application} the previously stored application instance under test
+*/
 export function getApplication() {
   return __application__;
 }
