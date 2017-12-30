c474d0fcec273a14990e81bcc9781f7fff0be774
Robert Jackson <rjackson@linkedin.com>
2017-12-29 22:26:39 -0500

    Docs: tweak examples for validateErrorHandler

--- addon-test-support/@ember/test-helpers/validate-error-handler.js
+++ addon-test-support/@ember/test-helpers/validate-error-handler.js
@@ -6,30 +6,28 @@ const INVALID = Object.freeze({
 });
 
 /**
-  Validate the provided error handler to confirm that it properly re-throws
-  errors when `Ember.testing` is true.
-
-  This is intended to be used by test framework hosts (or other libraries) to
-  ensure that `Ember.onerror` is properly configured. Without a check like
-  this, `Ember.onerror` could _easily_ swallow all errors and make it _seem_
-  like everything is just fine (and have green tests) when in reality
-  everything is on fire...
-
-  @example <caption>Example implementation for `ember-qunit`</caption>
-
-  ```js
-  import { validateErrorHandler } from '@ember/test-helpers';
-
-  test('Ember.onerror is functioning properly', function(assert) {
-    let result = validateErrorHandler();
-    assert.ok(result.isValid, result.message);
-  });
-  ```
-
-  @public
-  @param {Function} [callback=Ember.onerror] the callback to validate
-  @returns {Object} object with `isValid` and `message`
-*/
+ * Validate the provided error handler to confirm that it properly re-throws
+ * errors when `Ember.testing` is true.
+ *
+ * This is intended to be used by test framework hosts (or other libraries) to
+ * ensure that `Ember.onerror` is properly configured. Without a check like
+ * this, `Ember.onerror` could _easily_ swallow all errors and make it _seem_
+ * like everything is just fine (and have green tests) when in reality
+ * everything is on fire...
+ *
+ * @public
+ * @param {Function} [callback=Ember.onerror] the callback to validate
+ * @returns {Object} object with `isValid` and `message`
+ *
+ * @example <caption>Example implementation for `ember-qunit`</caption>
+ *
+ * import { validateErrorHandler } from '@ember/test-helpers';
+ *
+ * test('Ember.onerror is functioning properly', function(assert) {
+ *   let result = validateErrorHandler();
+ *   assert.ok(result.isValid, result.message);
+ * });
+ */
 export default function validateErrorHandler(callback = Ember.onerror) {
   if (callback === undefined || callback === null) {
     return VALID;
