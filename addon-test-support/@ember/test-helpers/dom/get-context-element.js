ea47e7868f6071ac21b78f63f3867a302f46e49e
Jasper Bingham <binghamjasper@gmail.com>
2018-04-03 11:24:32 -0400

    #351 Small JSDoc fix

--- addon-test-support/@ember/test-helpers/dom/get-context-element.js
+++ addon-test-support/@ember/test-helpers/dom/get-context-element.js
@@ -4,7 +4,7 @@ import getRootElement from './get-root-element';
  Used internally by the DOM interaction helpers to get the appropriate context element based on the context the user passes into the helper.
 
  @private
- @param {string|Element} context the context element or selector
+ @param {string|Element} [context] the context element or selector
  @returns {Element} the appropriate context element
  */
 export default function getContextElement(context) {
