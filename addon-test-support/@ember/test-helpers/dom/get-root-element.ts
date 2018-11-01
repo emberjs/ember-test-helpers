8ff229f8223b5c958e079c6e2877aa07e95aea3c
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-11-01 20:09:01 +0100

    dom/get-root-element: Use `isDocument/Element()` functions

--- addon-test-support/@ember/test-helpers/dom/get-root-element.ts
+++ addon-test-support/@ember/test-helpers/dom/get-root-element.ts
@@ -1,4 +1,5 @@
 import { getContext } from '../setup-context';
+import { isDocument, isElement } from './-target';
 
 /**
   Get the root element of the application under test (usually `#ember-testing`)
@@ -6,7 +7,7 @@ import { getContext } from '../setup-context';
   @public
   @returns {Element} the root element
 */
-export default function getRootElement(): Element {
+export default function getRootElement(): Element | Document {
   let context = getContext();
   let owner = context && context.owner;
 
@@ -23,11 +24,7 @@ export default function getRootElement(): Element {
     rootElement = '#ember-testing';
   }
 
-  if (
-    rootElement.nodeType === Node.ELEMENT_NODE ||
-    rootElement.nodeType === Node.DOCUMENT_NODE ||
-    rootElement instanceof Window
-  ) {
+  if (isElement(rootElement) || isDocument(rootElement) || rootElement instanceof Window) {
     return rootElement;
   } else if (typeof rootElement === 'string') {
     let _rootElement = document.querySelector(rootElement);
