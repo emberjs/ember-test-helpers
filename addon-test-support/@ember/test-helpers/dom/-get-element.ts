46425867c097d9f87eb5bd490aaefb060a36b9e4
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-11-01 18:28:33 +0100

    dom/-get-element: Switch condition order to make TypeScript happy

--- addon-test-support/@ember/test-helpers/dom/-get-element.ts
+++ addon-test-support/@ember/test-helpers/dom/-get-element.ts
@@ -1,4 +1,5 @@
 import getRootElement from './get-root-element';
+import Target from './-target';
 
 /**
   Used internally by the DOM interaction helpers to find one element.
@@ -7,17 +8,17 @@ import getRootElement from './get-root-element';
   @param {string|Element} target the element or selector to retrieve
   @returns {Element} the target or selector
 */
-export default function getElement(target) {
-  if (
+export default function getElement(target: Target) {
+  if (typeof target === 'string') {
+    let rootElement = getRootElement();
+
+    return rootElement.querySelector(target);
+  } else if (
     target.nodeType === Node.ELEMENT_NODE ||
     target.nodeType === Node.DOCUMENT_NODE ||
     target instanceof Window
   ) {
     return target;
-  } else if (typeof target === 'string') {
-    let rootElement = getRootElement();
-
-    return rootElement.querySelector(target);
   } else {
     throw new Error('Must use an element or a selector string');
   }
