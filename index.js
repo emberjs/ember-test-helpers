e52329bb06138eff90718971812ec94e90a5ae67
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-11-01 12:10:44 +0100

    Add `broccoli-debug` to `treeForAddonTestSupport()` hook

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const BroccoliDebug = require('broccoli-debug');
+const debugTree = BroccoliDebug.buildDebugCallback('ember-test-helpers');
+
 module.exports = {
   name: 'ember-test-helpers',
 
@@ -27,9 +30,13 @@ module.exports = {
     // so that can have our `import`'s be
     // import { ... } from 'ember-test-helpers';
 
-    return this.preprocessJs(tree, '/', this.name, {
+    let input = debugTree(tree, 'addon-test-support:input');
+
+    let output = this.preprocessJs(input, '/', this.name, {
       registry: this.registry,
     });
+
+    return debugTree(output, 'addon-test-support:output');
   },
 
   treeForVendor(rawVendorTree) {
