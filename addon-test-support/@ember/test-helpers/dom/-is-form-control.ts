4a2f4c2fd2a1fac2ec29b9a930a37cda1ce61ee5
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-11-01 18:33:38 +0100

    dom/-is-form-control: Add type declarations and simplify implementation

--- addon-test-support/@ember/test-helpers/dom/-is-form-control.ts
+++ addon-test-support/@ember/test-helpers/dom/-is-form-control.ts
@@ -5,12 +5,11 @@ const FORM_CONTROL_TAGS = ['INPUT', 'BUTTON', 'SELECT', 'TEXTAREA'];
   @param {Element} element the element to check
   @returns {boolean} `true` when the element is a form control, `false` otherwise
 */
-export default function isFormControl(element) {
-  let { tagName, type } = element;
-
-  if (type === 'hidden') {
-    return false;
-  }
-
-  return FORM_CONTROL_TAGS.indexOf(tagName) > -1;
+export default function isFormControl(
+  element: Element
+): element is HTMLInputElement | HTMLButtonElement | HTMLSelectElement | HTMLTextAreaElement {
+  return (
+    FORM_CONTROL_TAGS.indexOf(element.tagName) > -1 &&
+    (element as HTMLInputElement).type !== 'hidden'
+  );
 }
