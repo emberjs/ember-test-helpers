dd463a187303b2e64edf0db1cfa7633e0076f8bf
Steve Calvert <scalvert@linkedin.com>
2019-03-19 16:16:58 -0700

    Adding required jsdoc

--- addon-test-support/@ember/test-helpers/-internal/debug-info-helpers.ts
+++ addon-test-support/@ember/test-helpers/-internal/debug-info-helpers.ts
@@ -4,6 +4,12 @@ export interface DebugInfoHelper {
 
 export const debugInfoHelpers = new Set<DebugInfoHelper>();
 
+/**
+ * Registers a custom debug info helper to augment the output for test isolation validation.
+ *
+ * @public
+ * @param {DebugInfoHelper} debugHelper a custom debug info helper
+ */
 export default function registerDebugInfoHelper(debugHelper: DebugInfoHelper) {
   debugInfoHelpers.add(debugHelper);
 }
