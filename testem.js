b798c92bb3cc2f54445a047cd3e4632a9573cdda
Robert Jackson <rjackson@linkedin.com>
2020-04-22 14:41:35 -0400

    Increase browser start timeout.
    This matches a corresponding change in ember-cli's blueprints.

--- testem.js
+++ testem.js
@@ -4,6 +4,7 @@ module.exports = {
   disable_watching: true,
   launch_in_ci: ['Chrome', 'Firefox'],
   launch_in_dev: ['Chrome', 'Firefox'],
+  browser_start_timeout: 120,
   browser_args: {
     Chrome: {
       ci: [
