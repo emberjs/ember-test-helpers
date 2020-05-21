6b2c904f35a578b223a3469057495e6a3984da1b
Michael Kerr <Michael.r.kerr1@gmail.com>
2020-05-21 02:44:43 +0100

    Add `select()` helper (#728)
    * ISSUE-708: feat: add select helper
    
    * ISSUE-708: test: added test case for single option.
    
    * ISSUE-708: test: added one more test case and linting fixes
    
    * ISSUE-708: refactor: minor changes based of comments
    
    * ISSUE-708: feat: additional flag added and tests
    
    * ISSUE-708: linting fixes
    
    * ISSUE-708: feature: make clearing selected default and invert flag
    
    * ISSUE-708: Chore: linting fix and adding null check
    
    * ISSUE-708: refactor: update to error message and explicit default behaviour
    
    * ISSUE-708: chore: linting fixes
    
    * ISSUE-708: chore: updating to new linting rules
    
    * ISSUE-708: chore: changed append and events to work with IE 11

---
+++ addon-test-support/@ember/test-helpers/dom/select.ts
@@ -0,0 +1,85 @@
+import getElement from './-get-element';
+import isSelectElement from './-is-select-element';
+import { __focus__ } from './focus';
+import settled from '../settled';
+import fireEvent from './fire-event';
+import { nextTickPromise } from '../-utils';
+import Target from './-target';
+
+/**
+  Set the `selected` property true for the provided option the target is a
+  select element (or set the select property true for multiple options if the
+  multiple attribute is set true on the HTMLSelectElement) then trigger
+  `change` and `input` events on the specified target.
+
+  @public
+  @param {string|Element} target the element or selector for the select element
+  @param {string|string[]} options the value/values of the items to select
+  @param {boolean} keepPreviouslySelected a flag keep any existing selections
+  @return {Promise<void>} resolves when the application is settled
+
+  @example
+  <caption>
+    Emulating selecting an option or multiple options using `select`
+  </caption>
+
+  select('select', 'apple');
+
+  select('select', ['apple', 'orange']);
+
+  select('select', ['apple', 'orange'], true);
+*/
+export default function select(
+  target: Target,
+  options: string | string[],
+  keepPreviouslySelected = false
+): Promise<void> {
+  return nextTickPromise().then(() => {
+    if (!target) {
+      throw new Error('Must pass an element or selector to `select`.');
+    }
+
+    if (typeof options === 'undefined' || options === null) {
+      throw new Error('Must provide an `option` or `options` to select when calling `select`.');
+    }
+
+    const element = getElement(target);
+    if (!element) {
+      throw new Error(`Element not found when calling \`select('${target}')\`.`);
+    }
+
+    if (!isSelectElement(element)) {
+      throw new Error(`Element is not a HTMLSelectElement when calling \`select('${target}')\`.`);
+    }
+
+    if (element.disabled) {
+      throw new Error(`Element is disabled when calling \`select('${target}')\`.`);
+    }
+
+    options = Array.isArray(options) ? options : [options];
+
+    if (!element.multiple && options.length > 1) {
+      throw new Error(
+        `HTMLSelectElement \`multiple\` attribute is set to \`false\` but multiple options were passed when calling \`select('${target}')\`.`
+      );
+    }
+
+    __focus__(element);
+
+    for (let i = 0; i < element.options.length; i++) {
+      let elementOption = element.options.item(i);
+      if (elementOption) {
+        if (options.indexOf(elementOption.value) > -1) {
+          elementOption.selected = true;
+        } else if (!keepPreviouslySelected) {
+          elementOption.selected = false;
+        }
+      }
+    }
+
+    fireEvent(element, 'input');
+    fireEvent(element, 'change');
+
+    return settled();
+  });
+}
---
+++ tests/unit/dom/select-test.js
@@ -0,0 +1,210 @@
+import { module, test } from 'qunit';
+import { select, setupContext, teardownContext } from '@ember/test-helpers';
+import { buildInstrumentedElement } from '../../helpers/events';
+import { isIE11 } from '../../helpers/browser-detect';
+
+let selectSteps = ['focus', 'focusin', 'input', 'change'];
+let additionalSteps = ['input', 'change'];
+if (isIE11) {
+  selectSteps = ['focusin', 'focus', 'input', 'change'];
+}
+
+module('DOM Helper: select', function (hooks) {
+  let context, element;
+
+  hooks.beforeEach(function () {
+    context = {};
+  });
+
+  hooks.afterEach(async function () {
+    if (element) {
+      element.setAttribute('data-skip-steps', true);
+      element.parentNode.removeChild(element);
+    }
+    if (context.owner) {
+      await teardownContext(context);
+    }
+
+    document.getElementById('ember-testing').innerHTML = '';
+  });
+
+  test('select without target', async function (assert) {
+    await setupContext(context);
+    assert.rejects(select(), /Must pass an element or selector to `select`./);
+  });
+
+  test('select without options', async function (assert) {
+    element = buildInstrumentedElement('div');
+
+    await setupContext(context);
+
+    assert.rejects(
+      select(element, undefined),
+      /Must provide an `option` or `options` to select when calling `select`./
+    );
+    assert.rejects(
+      select(element, null),
+      /Must provide an `option` or `options` to select when calling `select`./
+    );
+  });
+
+  test('select with unfindable selector ', async function (assert) {
+    element = buildInstrumentedElement('div');
+    const option = 'example';
+
+    await setupContext(context);
+
+    assert.rejects(
+      select('#fake-selector', option),
+      /Element not found when calling `select\('#fake-selector'\)`/
+    );
+  });
+
+  test('select with element that is not a HTMLSelectElement', async function (assert) {
+    element = buildInstrumentedElement('input');
+
+    const option = 'example';
+
+    await setupContext(context);
+
+    assert.rejects(
+      select(element, option),
+      `Element is not a HTMLSelectElement when calling \`select(${element})\``
+    );
+  });
+
+  test('select with element that is disabled', async function (assert) {
+    element = buildInstrumentedElement('select');
+    element.disabled = true;
+    const option = 'example';
+
+    await setupContext(context);
+
+    assert.rejects(
+      select(element, option),
+      `Element is disabled when calling \`select(${element})\`./`
+    );
+  });
+
+  test('select | multiple: false - options.length > 1', async function (assert) {
+    element = buildInstrumentedElement('select');
+    element.multiple = false;
+    const options = ['apple', 'orange'];
+
+    await setupContext(context);
+
+    assert.rejects(
+      select(element, options),
+      `HTMLSelectElement \`multiple\` attribute is set to \`false\` but multiple options have been passed when calling \`select(${element})\``
+    );
+  });
+
+  test('select | 4 options - multiple: true - optionsToSelect.length : 2', async function (assert) {
+    const optionValues = ['apple', 'orange', 'pineapple', 'pear'];
+    element = buildInstrumentedElement('select');
+    element.multiple = true;
+
+    optionValues.forEach(optionValue => {
+      const optionElement = buildInstrumentedElement('option');
+      optionElement.value = optionValue;
+      element.appendChild(optionElement);
+    });
+
+    const optionsToSelect = ['apple', 'orange'];
+
+    await setupContext(context);
+
+    await select(element, optionsToSelect);
+
+    assert.verifySteps(selectSteps);
+    assert.equal(element.selectedOptions.length, 2);
+    assert.equal(element.selectedOptions[0].value, 'apple');
+    assert.equal(element.selectedOptions[1].value, 'orange');
+  });
+
+  test('select | 4 options - multiple: false - optionsToSelect.length : 1', async function (assert) {
+    const optionValues = ['apple', 'orange', 'pineapple', 'pear'];
+    element = buildInstrumentedElement('select');
+    element.multiple = false;
+
+    optionValues.forEach(optionValue => {
+      const optionElement = buildInstrumentedElement('option');
+      optionElement.value = optionValue;
+      element.appendChild(optionElement);
+    });
+
+    const optionsToSelect = 'apple';
+
+    await setupContext(context);
+
+    await select(element, optionsToSelect);
+
+    assert.verifySteps(selectSteps);
+    assert.equal(element.selectedIndex, 0);
+  });
+
+  test('select | multiple: false | select new option', async function (assert) {
+    const optionValues = ['apple', 'orange', 'pineapple', 'pear'];
+    element = buildInstrumentedElement('select');
+    element.multiple = false;
+
+    optionValues.forEach(optionValue => {
+      const optionElement = buildInstrumentedElement('option');
+      optionElement.value = optionValue;
+      element.appendChild(optionElement);
+    });
+
+    await setupContext(context);
+
+    await select(element, 'apple');
+
+    await select(element, 'orange');
+
+    assert.verifySteps([...selectSteps, ...additionalSteps]);
+    assert.equal(element.selectedIndex, 1);
+  });
+
+  test('select | multiple: true | keepPreviouslySelected: true', async function (assert) {
+    const optionValues = ['apple', 'orange', 'pineapple', 'pear'];
+    element = buildInstrumentedElement('select');
+    element.multiple = true;
+
+    optionValues.forEach(optionValue => {
+      const optionElement = buildInstrumentedElement('option');
+      optionElement.value = optionValue;
+      element.appendChild(optionElement);
+    });
+
+    await setupContext(context);
+
+    await select(element, 'apple');
+
+    await select(element, 'orange', true);
+
+    assert.verifySteps([...selectSteps, ...additionalSteps]);
+    assert.equal(element.selectedOptions[0].value, 'apple');
+    assert.equal(element.selectedOptions[1].value, 'orange');
+  });
+
+  test('select | multiple: true | keepPreviouslySelected: false', async function (assert) {
+    const optionValues = ['apple', 'orange', 'pineapple', 'pear'];
+    element = buildInstrumentedElement('select');
+    element.multiple = true;
+
+    optionValues.forEach(optionValue => {
+      const optionElement = buildInstrumentedElement('option');
+      optionElement.value = optionValue;
+      element.appendChild(optionElement);
+    });
+
+    await setupContext(context);
+
+    await select(element, 'apple');
+
+    await select(element, 'orange', false);
+
+    assert.verifySteps([...selectSteps, ...additionalSteps]);
+    assert.equal(element.selectedOptions[0].value, 'orange');
+    assert.equal(element.selectedOptions.length, 1);
+  });
+});
