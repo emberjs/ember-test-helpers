ea83820c3a0a6c6da72d8235af4d32497cdaefc6
Steve Calvert <scalvert@linkedin.com>
2020-06-05 09:49:52 -0700

    Renaming order param to label

--- addon-test-support/@ember/test-helpers/-internal/helper-hooks.ts
+++ addon-test-support/@ember/test-helpers/-internal/helper-hooks.ts
@@ -6,27 +6,28 @@ type HookUnregister = {
 const registeredHooks = new Map<string, Set<Hook>>();
 
 /**
- *
+ * @private
  * @param {string} helperName The name of the test helper in which to run the hook.
- * @param {string} order The order, either `start` or `end`, to run the hook.
+ * @param {string} label A label to help identify the hook.
  * @returns {string} The compound key for the helper.
  */
-function getHelperKey(helperName: string, order: string) {
-  return `${helperName}:${order}`;
+function getHelperKey(helperName: string, label: string) {
+  return `${helperName}:${label}`;
 }
 
 /**
  * Registers a hook function to be run during the invocation of a test helper.
  *
- * @public
+ * @private
  * @param {string} helperName The name of the test helper in which to run the hook.
- * @param {string} order The order, either `start` or `end`, to run the hook.
+ * @param {string} label A label to help identify the hook. Built-in labels are `start` and `end`,
+ *                       designating the start of the helper invocation and the end.
  * @param {Function} hook The hook function to run when the test helper is invoked.
  * @returns {HookUnregister} An object containing an unregister function that will unregister
  *                           the specific hook registered to the helper.
  */
-export function registerHook(helperName: string, order: string, hook: Hook): HookUnregister {
-  let helperKey = getHelperKey(helperName, order);
+export function registerHook(helperName: string, label: string, hook: Hook): HookUnregister {
+  let helperKey = getHelperKey(helperName, label);
   let hooksForHelper = registeredHooks.get(helperKey);
 
   if (hooksForHelper === undefined) {
@@ -46,14 +47,15 @@ export function registerHook(helperName: string, order: string, hook: Hook): Hoo
 /**
  * Runs all hooks registered for a specific test helper.
  *
- * @public
+ * @private
  * @param {string} helperName  The name of the test helper.
- * @param {string} order The order, either `start` or `end`, to run the hook.
+ * @param {string} label A label to help identify the hook. Built-in labels are `start` and `end`,
+ *                       designating the start of the helper invocation and the end.
  * @param {any[]} args Any arguments originally passed to the test helper.
  * @returns {Promise<void>} A promise representing the serial invocation of the hooks.
  */
-export function runHooks(helperName: string, order: string, ...args: any[]): Promise<void> {
-  let hooks = registeredHooks.get(getHelperKey(helperName, order)) || new Set<Hook>();
+export function runHooks(helperName: string, label: string, ...args: any[]): Promise<void> {
+  let hooks = registeredHooks.get(getHelperKey(helperName, label)) || new Set<Hook>();
   let promise = Promise.resolve();
 
   hooks.forEach(hook => promise.then(() => hook(...args)));
