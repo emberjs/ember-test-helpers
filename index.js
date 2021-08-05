6828d0c74a8f400d6ddaf7d089b9f9196db850a3
Stefan Penner <stefan.penner@gmail.com>
2021-08-05 17:02:14 -0600

    Make embroider testing work.
    - run floating deps + try scenarios in node 12. This is to minimize frustrating test failures due to package drift, and to allow for testing embroider in CI. While officially supporting node 10 when not using embroider
    - upgrade all other viable deps
    - move to simple tsc -b approach
    - only run embroider-safe, as embroider-optimized is not yet compatible with our tests

--- index.js
+++ index.js
@@ -7,33 +7,16 @@ module.exports = {
   name: require('./package').name,
 
   treeForAddonTestSupport(tree) {
-    // intentionally not calling _super here
-    // so that can have our `import`'s be
-    // import { ... } from 'ember-test-helpers';
-
     let input = debugTree(tree, 'addon-test-support:input');
 
-    let compiler = this.project._incrementalTsCompiler;
-    if (this.isDevelopingAddon() && compiler) {
-      // eslint-disable-next-line node/no-unpublished-require
-      let TypescriptOutput = require('ember-cli-typescript/js/lib/incremental-typescript-compiler/typescript-output-plugin');
-      // eslint-disable-next-line node/no-unpublished-require
-      let MergeTrees = require('broccoli-merge-trees');
-
-      let tsTree = debugTree(
-        new TypescriptOutput(compiler, {
-          'addon-test-support/@ember/test-helpers': '@ember/test-helpers',
-        }),
-        'addon-test-support:ts'
-      );
-
-      input = debugTree(
-        new MergeTrees([input, tsTree]),
-        'addon-test-support:merged'
-      );
+    if (this.isDevelopingAddon()) {
+      const {
+        BroccoliBabelPresetTypeScript,
+      } = require('broccoli-babel-preset-typescript'); // eslint-disable-line node/no-unpublished-require
+      input = new BroccoliBabelPresetTypeScript([input]);
     }
 
-    let output = this.preprocessJs(input, '/', this.name, {
+    const output = this.preprocessJs(input, '/', this.name, {
       registry: this.registry,
       treeType: 'addon-test-support',
     });
