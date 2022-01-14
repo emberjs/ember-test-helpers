ab797ef4fc6c99436983ddba27c3591e50e504df
Shane Martin <dev.sh@nemartin>
2022-01-14 10:49:46 -0500

    test: add more/improve register hooks helpers

--- tests/helpers/register-hooks.js
+++ tests/helpers/register-hooks.js
@@ -1,6 +1,33 @@
 import { _registerHook } from '@ember/test-helpers';
 
 /**
+ * Register mock hooks for a helper and optional list of expected events performed while the helper is executed.
+ *
+ * @param {Assert} assert Test assertion context
+ * @param {string} helperName Helper name
+ * @param {Object} [options] Options object
+ * @param {string[]} [options.eventTypes] Event types to register
+ * @returns {HookUnregister[]} Unregisterable hooks
+ */
+export const registerHooks = (assert, helperName, { eventTypes } = {}) => {
+  const mockHooks = [
+    _registerHook(helperName, 'start', () => {
+      assert.step(`${helperName}:start`);
+    }),
+    _registerHook(helperName, 'end', () => {
+      assert.step(`${helperName}:end`);
+    }),
+  ];
+
+  if (Array.isArray(eventTypes)) {
+    const fireEventHooks = registerFireEventHooks(assert, eventTypes);
+    mockHooks.push(...fireEventHooks);
+  }
+
+  return mockHooks;
+};
+
+/**
  * Register mock `fireEvent` hooks for provided event types.
  *
  * @param {Assert} assert Test assertion context
@@ -15,7 +42,7 @@ export const registerFireEventHooks = (assert, eventTypes) => {
     assert.step(`fireEvent:end`);
   });
 
-  const eventSpecificHooks = eventTypes.flatMap((eventType) => [
+  const eventSpecificHooks = [...new Set(eventTypes)].flatMap((eventType) => [
     _registerHook(`fireEvent:${eventType}`, 'start', () => {
       assert.step(`fireEvent:${eventType}:start`);
     }),
@@ -39,19 +66,36 @@ export const unregisterHooks = (hooks) => {
 /**
  * Build expected `fireEvent` steps for verification.
  *
- * @param {string[]} eventTypes List of eventTypes
- * @return {string[]} List of expected `fireEvent` steps
+ * @param {string[]} eventTypes Event types
+ * @return {string[]} Expected executed `fireEvent` steps
  */
-export const buildFireEventSteps = (eventTypes) =>
-  eventTypes.flatMap((eventType) => [
+export const buildExpectedFireEventSteps = (eventTypes) =>
+  eventTypes?.flatMap((eventType) => [
     'fireEvent:start',
     `fireEvent:${eventType}:start`,
     `fireEvent:end`,
     `fireEvent:${eventType}:end`,
   ]);
 
+/**
+ * Build list of expected executed steps for verification.
+ *
+ * @param {string} helperName Helper name
+ * @param {Object} [options] Options object
+ * @param {string[]} [options.eventTypes] Event types to register
+ * @return {string[]} Expected executed steps
+ */
+export const buildExpectedSteps = (helperName, { eventTypes } = {}) =>
+  [
+    `${helperName}:start`,
+    ...buildExpectedFireEventSteps(eventTypes),
+    `${helperName}:end`,
+  ].filter(Boolean);
+
 export default {
+  registerHooks,
   registerFireEventHooks,
-  buildFireEventSteps,
+  buildExpectedSteps,
+  buildExpectedFireEventSteps,
   unregisterHooks,
 };
