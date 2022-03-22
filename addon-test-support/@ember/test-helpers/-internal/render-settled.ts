d0767e940b6a91ebef2c47f97ca53aa8b17ec001
Chris Freeman <cfreeman.code@gmail.com>
2022-03-22 10:31:55 -0600

    streamline renderSettled ponyfill

--- addon-test-support/@ember/test-helpers/-internal/render-settled.ts
+++ addon-test-support/@ember/test-helpers/-internal/render-settled.ts
@@ -10,24 +10,13 @@ let renderSettled: () => Promise<void>;
 if (macroCondition(dependencySatisfies('ember-source', '>=4.4.0'))) {
   //@ts-ignore
   renderSettled = importSync('@ember/renderer').renderSettled;
-} else if (macroCondition(dependencySatisfies('ember-source', '>=4.0.0'))) {
-  //@ts-ignore
-  renderSettled = importSync('@ember/-internals/glimmer').renderSettled;
 } else if (macroCondition(dependencySatisfies('ember-source', '>=3.27.0'))) {
   //@ts-ignore
   renderSettled = importSync('@ember/-internals/glimmer').renderSettled;
-} else if (macroCondition(dependencySatisfies('ember-source', '>=3.6.0'))) {
+} else {
   renderSettled = (Ember as any).__loader.require(
     '@ember/-internals/glimmer'
   ).renderSettled;
-} else if (macroCondition(dependencySatisfies('ember-source', '>=3.0.0'))) {
-  renderSettled = renderSettled = (Ember as any).__loader.require(
-    'ember-glimmer'
-  ).renderSettled;
-} else {
-  throw new Error(
-    'renderSettled is not available before v3.0 of ember-source.'
-  );
 }
 
 export default renderSettled;
