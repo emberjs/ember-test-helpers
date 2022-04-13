baa2bc983ac84da76239c4e884f250cbb8f25d60
Chris Freeman <cfreeman.code@gmail.com>
2022-04-13 10:52:43 -0600

    add note isComponent helper in docs

--- addon-test-support/@ember/test-helpers/-internal/is-component.ts
+++ addon-test-support/@ember/test-helpers/-internal/is-component.ts
@@ -3,7 +3,8 @@ import { macroCondition, dependencySatisfies } from '@embroider/macros';
 import getComponentManager from './get-component-manager';
 
 /**
- *
+ * We should ultimately get a new API from @glimmer/runtime that provides this functionality
+ * (see https://github.com/emberjs/rfcs/pull/785 for more info).
  * @private
  * @param {Object} maybeComponent The thing you think might be a component
  * @param {Object} owner Owner, we need this for old versions of getComponentManager
