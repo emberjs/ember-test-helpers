799e02f9403e73e20ad5c91fecaf687c40a8cb85
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-03-24 17:49:35 -0400

    Use old ember-try config

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -1,30 +1,46 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
-const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
+const latestVersion = require('latest-version');
 
 module.exports = async function () {
-  let releaseVersion = await getChannelURL('release');
+  const EMBROIDER_VERSION = await latestVersion('@embroider/core');
+
+  const embroider = {
+    safe: {
+      name: 'embroider-safe',
+      npm: {
+        devDependencies: {
+          '@embroider/core': EMBROIDER_VERSION,
+          '@embroider/webpack': EMBROIDER_VERSION,
+          '@embroider/compat': EMBROIDER_VERSION,
+          '@embroider/test-setup': EMBROIDER_VERSION,
+        },
+      },
+      env: {
+        EMBROIDER_TEST_SETUP_OPTIONS: 'safe',
+      },
+    },
+
+    optimized: {
+      name: 'embroider-optimized',
+      npm: {
+        devDependencies: {
+          '@embroider/core': EMBROIDER_VERSION,
+          '@embroider/webpack': EMBROIDER_VERSION,
+          '@embroider/compat': EMBROIDER_VERSION,
+          '@embroider/test-setup': EMBROIDER_VERSION,
+        },
+      },
+      env: {
+        EMBROIDER_TEST_SETUP_OPTIONS: 'optimized',
+      },
+    },
+  };
 
   return {
-    usePnpm: true,
+    useYarn: true,
     scenarios: [
-      {
-        name: 'ember-lts-3.28',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-        },
-      },
-      {
-        name: 'ember-4.0',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.0.0',
-          },
-        },
-      },
       {
         name: 'ember-lts-4.4',
         npm: {
@@ -45,7 +61,7 @@ module.exports = async function () {
         name: 'ember-release',
         npm: {
           devDependencies: {
-            'ember-source': releaseVersion,
+            'ember-source': await getChannelURL('release'),
           },
         },
       },
@@ -65,46 +81,27 @@ module.exports = async function () {
           },
         },
       },
-      embroiderSafe({
-        name: 'ember-release + embroider-safe',
+      {
+        name: 'ember-default',
         npm: {
-          devDependencies: {
-            'ember-source': releaseVersion,
-          },
+          devDependencies: {},
         },
-      }),
-      embroiderOptimized({
-        name: 'ember-release + embroider-optimized',
-        npm: {
-          devDependencies: {
-            'ember-source': releaseVersion,
-          },
-        },
-      }),
-      embroiderOptimized({
-        name: 'ember-lts-3.28 + embroider-optimized',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-        },
-      }),
-      embroiderOptimized({
-        name: 'ember-lts-4.4 + embroider-optimized',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.4.0',
-          },
-        },
-      }),
-      embroiderOptimized({
-        name: 'ember-lts-4.8 + embroider-optimized',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.8.0',
-          },
-        },
-      }),
+      },
+      embroider.safe,
+      // disable embroider optimized test scenarios, as the dynamism these
+      // tests use is not compatible with embroider we are still exploring
+      // appropriate paths forward.
+      //
+      // Steps to re-enable:
+      //
+      // 1. have a strategy to make this work
+      // 2. uncomment the next line
+      // embroider.optimized,
+      //
+      // 3. add "embroider-optimized" to .github/workflows/ci-build.yml's
+      //    ember-try-scenario list.
+      //
     ],
   };
 };
+
