a983566fbbb9b4b3b5718b0e03c340c1a9c73684
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-03-24 17:47:37 -0400

    Remove ember-data and add test-app to the workspaces list

--- test-app/types/global.d.ts
+++
@@ -1,7 +0,0 @@
-// Types for compiled templates
-declare module 'test-app/templates/*' {
-  import { TemplateFactory } from 'ember-cli-htmlbars';
-
-  const tmpl: TemplateFactory;
-  export default tmpl;
-}
