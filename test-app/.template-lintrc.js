99f46c88a62d6e131dbf01a247d1cd413128e513
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-03-24 23:01:54 -0400

    Addressing linting mismatch between the old test suite and and the new lints

--- test-app/.template-lintrc.js
+++ test-app/.template-lintrc.js
@@ -2,4 +2,21 @@
 
 module.exports = {
   extends: 'recommended',
+
+  overrides: [
+    {
+      // TODO: resolve these in a followup PR
+      files: ['**/*.{js,ts}'],
+      rules: {
+        'no-empty-headings': 'off',
+        'no-inline-styles': 'off',
+        'no-curly-component-invocation': 'off',
+        'require-input-label': 'off',
+        'require-button-type': 'off',
+        'no-link-to-positional-params': 'off',
+        'no-unnecessary-component-helper': 'off',
+        'no-action': 'off',
+      },
+    },
+  ],
 };
