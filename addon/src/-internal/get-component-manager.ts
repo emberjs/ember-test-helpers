92764a9ad2e8db5654efe61030ed83a3cd7b4b82
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-05-01 22:57:35 -0400

    Maybe ember-source needs to be converted to v2 first, given this silliness

--- addon/src/-internal/get-component-manager.ts
+++ addon/src/-internal/get-component-manager.ts
@@ -1,38 +1,6 @@
 import Ember from 'ember';
-import {
-  macroCondition,
-  importSync,
-  dependencySatisfies,
-} from '@embroider/macros';
+// @ts-ignore
+// import { getInternalComponentManager as getComponentManager } from '@glimmer/manager';
 
-let getComponentManager: (definition: object, owner: object) => unknown;
-
-if (macroCondition(dependencySatisfies('ember-source', '>=3.27.0-alpha.1'))) {
-  let _getComponentManager =
-    //@ts-ignore
-    importSync('@glimmer/manager').getInternalComponentManager;
-
-  getComponentManager = (definition: object, owner: object) => {
-    return _getComponentManager(definition, true);
-  };
-} else if (
-  macroCondition(dependencySatisfies('ember-source', '>=3.25.0-beta.1'))
-) {
-  let _getComponentManager = (Ember as any).__loader.require(
-    '@glimmer/manager'
-  ).getInternalComponentManager;
-
-  getComponentManager = (definition: object, owner: object) => {
-    return _getComponentManager(definition, true);
-  };
-} else {
-  let _getComponentManager = (Ember as any).__loader.require(
-    '@glimmer/runtime'
-  ).getComponentManager;
-
-  getComponentManager = (definition: object, owner: object) => {
-    return _getComponentManager(owner, definition);
-  };
-}
-
-export default getComponentManager;
+// export default getComponentManager;
+export default Ember._getComponentManager;
