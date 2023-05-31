c453f8cbf5df679c1dc89fdfa022e3ce29328210
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-05-30 20:49:43 -0400

    Fix the TS error

--- addon/addon-test-support/@ember/test-helpers/-internal/is-component.ts
+++ addon/addon-test-support/@ember/test-helpers/-internal/is-component.ts
@@ -14,7 +14,10 @@ function isComponent(
   maybeComponent: object,
   owner: object
 ): maybeComponent is ComponentLike {
-  return !!getComponentManager(maybeComponent, owner);
+  // SAFETY: because we're not dropping support for the `@glimmer/manager`
+  //         version that requires the owner, *but* we are using the types from
+  //         the newer `@glimmer/manager`, we have to cast to a more permissive type.
+  return !!(getComponentManager as any)(maybeComponent, owner);
 }
 
 export default isComponent;
