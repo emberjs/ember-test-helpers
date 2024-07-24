4a900612b82387f51186b9fb28a22aa021ea97f8
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-07-24 13:58:51 -0400

    Holy lint violations, batman

--- test-app/.eslintrc.js
+++ test-app/.eslintrc.js
@@ -49,8 +49,38 @@ module.exports = {
     },
     {
       // test files
-      files: ['tests/**/*-test.{js,ts}'],
+      files: ['tests/**/*'],
       extends: ['plugin:qunit/recommended'],
+      rules: {
+        // TODO items from upgrades (but also, test-helpers may need to support all these things)
+
+        // eslint-plugin-ember
+        'ember/avoid-leaking-state-in-ember-objects': 'off',
+        'ember/new-module-imports': 'off',
+        'ember/no-actions-hash': 'off',
+        'ember/no-classic-classes': 'off',
+        'ember/no-classic-components': 'off',
+        'ember/no-ember-testing-in-module-scope': 'off',
+        'ember/no-empty-glimmer-component-classes': 'off',
+        'ember/no-get': 'off',
+        'ember/require-super-in-lifecycle-hooks': 'off',
+        'ember/no-global-jquery': 'off',
+        'ember/no-jquery': 'off',
+        'ember/no-legacy-test-waiters': 'off',
+        'ember/no-pause-test': 'off',
+        'ember/no-settled-after-test-helper': 'off',
+        'ember/require-valid-css-selector-in-test-helpers': 'off',
+
+        // eslint-plugin-qunit
+        'qunit/no-assert-equal': 'off',
+        'qunit/no-assert-equal-boolean': 'off',
+        'qunit/no-conditional-assertions': 'off',
+        'qunit/no-early-return': 'off',
+        'qunit/no-identical-names': 'off',
+        'qunit/no-negated-ok': 'off',
+        'qunit/require-expect': 'off',
+        'qunit/resolve-async': 'off',
+      },
     },
   ],
 };
--- test-app/.template-lintrc.js
+++ test-app/.template-lintrc.js
@@ -2,4 +2,20 @@
 
 module.exports = {
   extends: 'recommended',
+  overrides: [
+    {
+      files: ['tests/**/*'],
+      rules: {
+        // These probably don't need to be enabled for tests
+        'no-action': 'off',
+        'no-curly-component-invocation': 'off',
+        'no-empty-headings': 'off',
+        'no-inline-styles': 'off',
+        'no-link-to-positional-params': 'off',
+        'no-unnecessary-component-helper': 'off',
+        'require-button-type': 'off',
+        'require-input-label': 'off',
+      },
+    },
+  ],
 };
