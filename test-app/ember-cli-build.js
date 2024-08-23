93319ad5eeed437680d2ada6a9b9489f7b56f174
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-08-23 10:43:40 -0400

    Add packageRules

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -8,5 +8,23 @@ module.exports = function (defaults) {
   });
 
   const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app);
+  return maybeEmbroider(app, {
+    packageRules: [
+      {
+        package: 'test-app',
+        components: {
+          'x-test-1': { safeToIgnore: true },
+          'x-test-2': { safeToIgnore: true },
+          'x-test-3': { safeToIgnore: true },
+          'x-test-4': { safeToIgnore: true },
+          'x-test-5': { safeToIgnore: true },
+          'foo-bar': { safeToIgnore: true },
+          'x-foo': { safeToIgnore: true },
+          'x-input': { safeToIgnore: true },
+          'my-input': { safeToIgnore: true },
+          'my-component': { safeToIgnore: true },
+        },
+      },
+    ],
+  });
 };
